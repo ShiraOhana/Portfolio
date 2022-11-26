518e66e08e0ac76208e161113f1722136b13c0c5
ShiraOhana <soshiraohana@gmail.com>
2022-11-26 22:30:22 +0200

    Main projects change

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // scroll to elements
 
 const links = document.querySelectorAll(".nav-link"); // select all nav bar links
-console.log(links)
+console.log(links);
 for (const link of links) {
   // foreach link of links
   link.addEventListener("click", (event) => {
@@ -19,7 +19,6 @@ for (const link of links) {
     });
   });
 }
-
 
 //dark mode
 
@@ -73,7 +72,6 @@ function lightMode() {
   darkModeButton.setAttribute("title", "Turn lights off");
 }
 
-
 // scroll to top arrow
 const hero = document.querySelector("#hero");
 const scrollTop = document.querySelector("#scroll-top");
@@ -90,105 +88,3 @@ window.addEventListener("scroll", () => {
     scrollTop.style.visibility = "";
   }
 });
-// get git hub api response
-const myProjectsLink= document.querySelector("#navbarSupportedContent > ul > li:nth-child(2) > a");
-const myProjectsSection = document.querySelector("#projects");
-const myProjectsItems = document.querySelector("#projects-items");
-
-fetch("https://api.github.com/users/ineserdoura/repos")
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    for (const repo of data) {
-      console.log(data);
-      console.log(data.indexOf(repo));
-
-      // foreach repositorie from GitHub
-      const repositoryUrl = repo.html_url; // repositorie url
-      const repositoryName = repo.name.replaceAll("-", " ").toUpperCase(); // removes the - that exist in the repositorie names
-      const projectWebsite = repo.homepage; // project website
-      const repositoryLanguages = repo.languages_url;
-      const descripion = repo.description;
-      // create col
-      const col = document.createElement("div");
-      col.classList.add("col-md-4", "col-12", "mt-3");
-
-      //create card
-      const card = document.createElement("div");
-      card.classList.add("card", "text-center");
-
-      //create card body
-      const cardBody = document.createElement("div");
-      cardBody.classList.add("card-body");
-
-      //create card title
-      const cardTitle = document.createElement("h6");
-      cardTitle.textContent = `${repositoryName}`;
-
-      //create card subtitle / repo website
-      const ProjectLiveUrl = document.createElement("a");
-      ProjectLiveUrl.classList.add("mb-2");
-      ProjectLiveUrl.setAttribute("href", projectWebsite);
-      ProjectLiveUrl.setAttribute("target", "_blank");
-      ProjectLiveUrl.textContent = "Check the project live.";
-      if (!projectWebsite) {
-        ProjectLiveUrl.style.visibility = "hidden"; // if the project doesn't have a website, hide this section
-      }
-
-      //create card descripion
-      const cardDescription = document.createElement("p");
-      cardDescription.innerHTML = `${descripion}`;
-
-      // create card text / languages icons
-      const cardLanguages = document.createElement("h3");
-      cardLanguages.classList.add("card-text");
-
-      //create card link / repository link
-      const h3 = document.createElement("h3");
-      const cardLink = document.createElement("a");
-      cardLink.classList.add("card-link");
-      cardLink.setAttribute("href", repositoryUrl);
-      cardLink.setAttribute("target", "_blank");
-
-      //create github icon
-      const gitHubIcon = document.createElement("i");
-      gitHubIcon.classList.add("fab", "fa-github");
-      cardLink.appendChild(gitHubIcon); // append github icon to link
-      h3.appendChild(cardLink); // append link to h3
-
-      // create card
-      cardBody.appendChild(cardTitle); // append title to card body
-
-      cardBody.appendChild(cardLanguages); // append text to card body
-      cardBody.appendChild(h3); // append h3 to card body
-      cardBody.appendChild(ProjectLiveUrl); // append subtitle to card body
-      cardBody.appendChild(cardDescription); // append description to card body
-      card.appendChild(cardBody); // append card body to card
-      col.appendChild(card); // append card to col
-      myProjectsItems.appendChild(col); // append col to projects section
-      const repoPosition = data.indexOf(repo);
-      if (repoPosition % 2 == 0 || repoPosition == 0) {
-        card.classList.add("card-even");
-      } else {
-        card.classList.add("card-odd");
-      }
-
-      fetch(repositoryLanguages) // get repository languages url response
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          const repoLanguages = Object.keys(data).sort(); // create an array with the keys from the repository languages object
-          const h6 = document.createElement("h6");
-          h6.classList.add("px-1");
-          h6.innerHTML = repoLanguages.join(" "); // create a string from the previous array
-          cardLanguages.appendChild(h6); // append h6 to card text
-        });
-    }
-  })
-  .catch(() => {
-    // if no response/ error from the api hide my projects link on menu and my projects section
-    myProjectsLink.style.display="none";
-    myProjectsSection.style.display = "none";
-  });
